92b142b7d8c15643ff4ec5c3c9849f103d203cc9
Aditya Narayn <aditya.narayn30@gmail.com>
2020-11-16 23:42:49 +0530

    Toggle For Dark mode

---
+++ contextapp/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <ThemeContext.Consumer>{(context) => {
+            const { isLightTheme, light, dark } = context;  //accessing context- this way helps multiple context
+            const theme = isLightTheme ? light : dark;
+                return(<nav style={{background: theme.ui, color: theme.syntax}}>
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        </ul>
+                    </nav>
+                    )
+            }}
+            </ThemeContext.Consumer>
+         );
+    }
+}
+ 
+export default Navbar;
