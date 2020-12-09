823e49b2302acddf196679cefbfb333051e51317
Aditya Narayn <aditya.narayn30@gmail.com>
2020-12-09 16:44:58 +0530

    final

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -3,14 +3,14 @@ import BookContextPreovider from "./context/BookContext";
 import Navbar from "./components/Navbar";
 import "./styles.css";
 import BookList from "./components/BookList";
-import NewBookForm from "./components/BookForm";
+import NewBookForm from "./components/NewBookForm";
 
 export default function App() {
   return (
     <div className="App">
       <BookContextPreovider>
         <Navbar />
-        <BookList/>
+        <BookList />
         <NewBookForm />
       </BookContextPreovider>
     </div>
--- reading-list/src/context/BookContext.js
+++ reading-list/src/context/BookContext.js
@@ -1,27 +1,21 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid'
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
-const BookContextPreovider = (props) => {
-   const [books, setBooks] = useState([
-    {title: 'The Da Vinci Code', author:'Dan Brown', id: 1 },
-    {title: 'Harry Potter Series', author:'J K Rowling', id: 2},
-    {title: 'Secret Seven', author:'Enid Blyton',id:3}
-   ]);
-   const addBook = (title, author) => {
-   setBooks ([...books, {title, author, id: uuid }])
-   };
-
-   const removeBook = (id) => {
-   setBooks(books.filter(book => book.id !== id));
-};
-return(
-  <BookContext.Provider value={{books, addBook, removeBook}}>
-    {props.children}
+const BookContextProvider = (props) => {
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
+  return (
+    <BookContext.Provider value={{ books, dispatch }}>
+      {props.children}
     </BookContext.Provider>
-
- )
+  );
 }
-
-export default BookContextPreovider;
+ 
+export default BookContextProvider;
